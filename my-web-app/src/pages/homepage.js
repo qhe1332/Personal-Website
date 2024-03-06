f339d0a2ed2d65ea4a197177e7de452d470d6b39
qhe1332 <cherryhe5@gmail.com>
2024-03-05 21:39:48 -0500

    added shin-chan

--- my-web-app/src/pages/homepage.js
+++ my-web-app/src/pages/homepage.js
@@ -1,23 +1,34 @@
-import React from "react";
-import { Parallax, ParallaxLayer } from "@react-spring/parallax"
-import { BrowserRouter as Router, Routes, Route, Link, NavLink } from "react-router-dom";
-import Navbar from "../components/navbar";
+import React, { useState, useEffect }from "react";
+import { Parallax, ParallaxLayer } from "@react-spring/parallax";
 import cloud1 from "../assets/cloud1.png";
 import cloud2 from "../assets/cloud2.png";
+import shin from "../assets/shin.png";
+import shinShadow from "../assets/shinShadow.png";
+const Navbar = React.lazy(() => import("../components/navbar"));
 
 function Homepage(prop)
 {   
     return(  
         <div className="page">
-            <Parallax pages={2} style={{ top: '0', left: '0' }} className="animation">
-                <ParallaxLayer offset={0} speed={2.5}>
+        <div className="home">
+            <Parallax pages={3} style={{ top: '0', left: '0' }} className="animation">
+                <ParallaxLayer offset={0} speed={2.0}>
                     <div className="opening"></div>
                 </ParallaxLayer>
                 <ParallaxLayer offset={0} speed={1}>
                     <div className="intro">
-                        <h1 className="title">Hello, I'm Cherry!</h1> 
+                        <div className="intro-top">
+                            <h1 className="title">Hello, I'm Cherry!</h1> 
+                            <span id="shin">
+                                <img className="pixelart" id="shinShadow" src={shinShadow}/>
+                                <img className="pixelart" id="shinSpritesheet" src={shin}/>
+                            </span>
+                        </div>
                         <h4>I'm a self-motivated <b>WEB DEVELOPER</b> who  is passionate about building user-centered applications. I am familar with React.js, HTML/CSS, Javascript, p5.js, Unity, and Java. I also have skills in Adobe Suite.</h4>
                     </div>
+                </ParallaxLayer>
+                <ParallaxLayer offset={0} speed={1}>
+                    
                 </ParallaxLayer>
                 <ParallaxLayer offset={0} speed={1.7}>
                     <div className="animation-layer" id="cloud1"></div>
@@ -31,10 +42,17 @@ function Homepage(prop)
                 <ParallaxLayer offset={0} speed={1}>
                     <Navbar/>
                 </ParallaxLayer>
-
+                {/* <ParallaxLayer offset={1} speed={2.0}>
+                    <div className="projects"></div>
+                </ParallaxLayer>
+                <ParallaxLayer offset={2} speed={2.0}>
+                    <div className="journey"></div>
+                </ParallaxLayer> */}
 
             </Parallax>
-        </div>  
+            
+        </div> 
+        </div> 
     );
 }
 
